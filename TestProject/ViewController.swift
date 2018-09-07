69d12c24fe67bcbbf971a4eac308d0705a5e75e9
shan <shanmugam.b@technoduce.com>
2018-09-07 12:38:30 +0530

    Updated test

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Apple on 07/09/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
